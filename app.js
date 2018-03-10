485f8bf2789a781377fb2421c25a275191480a84
Dibakar <crossbonesdipu@gmail.com>
2018-03-10 22:13:50 +0800

    Dice display to none at the start of the gamae

--- app.js
+++ app.js
@@ -4,8 +4,16 @@ scores = [0, 0];
 roundScore = 0;
 activePlayer = 1;
 
-dice = Math.floor(Math.random() * 6 ) + 1;
-console.log(dice);
+dice = Math.floor(Math.random() * 6 ) + 1;															// Random Dice Number Generate
+//console.log(dice);
 
-document.querySelector ('#current-' + activePlayer).textContent = dice;
+document.querySelector ('#current-' + activePlayer).textContent = dice;								// setter - sets a value
 //document.querySelector ('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
+
+/*
+var x = document.querySelector('#score-0').textContent;												// getter - gets a value
+console.log(x);																						// just an example
+*/
+
+
+document.querySelector ('.dice').style.display = 'none';											// For not showing the dice picture at the start of the game
