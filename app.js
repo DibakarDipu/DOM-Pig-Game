01b3b796eb6cf6054b98b02f007828133ca85212
Dibakar <crossbonesdipu@gmail.com>
2018-03-11 20:29:31 +0800

    DRY Principle Applied

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var scores, roundScore, activePlayer;
 
 scores = [0, 0];
 roundScore = 0;
-activePlayer = 1;
+activePlayer = 0;
 
 document.querySelector ('.dice').style.display = 'none';											// For not showing the dice picture at the start of the game
 
@@ -22,11 +22,49 @@ document.querySelector ('.btn-roll').addEventListener ('click', function() {				
 	diceDOM.src = 'dice-' + dice + '.png';
 
 	// 3. Update the Round Score IF the rolled number was NOT a 1
+	if ( dice !== 1){
+		// ADD Score
+		roundScore += dice;
+		document.querySelector ('#current-' + activePlayer).textContent = roundScore;
+	} else {
+		// Next Player
+		nextPlayer();
+	}
+});
 
+document.querySelector ('.btn-hold').addEventListener ('click', function(){
+
+	// Add CURRENT Score to GLOBAL Score
+	scores[activePlayer] += roundScore;
+
+	// Update the UI
+	document.querySelector ('#score-' + activePlayer).textContent = scores[activePlayer];
+
+	// Check IF the player WON the game
+
+
+	// Next Player
+	nextPlayer();
 
 });
 
 
+function nextPlayer(){
+	// Next Player
+		activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;									// Ternary Operator - A clean and simple version of IfElse
+		roundScore = 0;
+
+		document.getElementById ('current-0').textContent = '0';
+		document.getElementById ('current-1').textContent = '0';
+
+		document.querySelector ('.player-0-panel').classList.toggle ('active');
+		document.querySelector ('.player-1-panel').classList.toggle ('active');
+
+		//document.querySelector ('.player-0-panel').classList.remove ('active');
+		//document.querySelector ('.player-1-panel').classList.add ('active');
+
+		document.querySelector ('.dice').style.display = 'none';
+}
 
 
 /*
