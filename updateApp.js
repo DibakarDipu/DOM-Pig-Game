fc67638a38d2a2b2f66966e13578c654aa519cf8
Dibakar <crossbonesdipu@gmail.com>
2018-03-13 18:03:25 +0800

    User input Final Score Done

--- updateApp.js
+++ updateApp.js
@@ -43,8 +43,19 @@ document.querySelector ('.btn-hold').addEventListener ('click', function(){
 		// Update the UI
 		document.querySelector ('#score-' + activePlayer).textContent = scores[activePlayer];
 
+		var input = document.querySelector ('.final-score').value;
+		var winningScore;
+
+		// Undefined, null or 0, "" are CORECED to FALSE
+		// Anything else are CORECED to TRUE
+		if (input){
+			winningScore = input;
+		} else {
+			winningScore = 100;
+		}
+
 		// Check IF the player WON the game
-		if (scores[activePlayer] >= 100) {
+		if (scores[activePlayer] >= winningScore) {
 			document.querySelector ('#name-' + activePlayer).textContent = 'WINNER !';
 			document.querySelector ('.dice').style.display = 'none';
 			document.querySelector ('.player-' + activePlayer + '-panel').classList.add('winner');
